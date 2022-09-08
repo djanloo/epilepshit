fbee9ee6f1d8f17d5c200e4fbd262ab8b0643e86
djanloo <zzibecu@gmail.com>
2022-09-09 00:53:27 +0200

    Started using a dictionary of frames for #1

--- animate.py
+++ animate.py
@@ -9,13 +9,34 @@ from matplotlib import pyplot as plt
 ACTIVITY = "metastable"
 EXCFILE = f"binned_excitatory_{ACTIVITY}.json"
 INHFILE =  f"binned_inhibitory_{ACTIVITY}.json"
+TIMESTEP = 0.01 #us
 
 class Normal(undNetwork):
 
     def __init__(self):
 
-        ## Initializes a random network of 500 nodes with density 20%
-        self.net = undNetwork.Random(500, 0.2)
+        # Try to visualize the inhibitory and excitatory neurons
+        # as two distinct clusters
+        # Build a full-two-cluster adj matrix then kill some element
+        adjacency_matrix = np.zeros((500, 500), dtype=np.float32)
+        adjacency_matrix[0:400, 0:400] = np.ones((400 ,400), dtype=np.float32)
+        adjacency_matrix[400:500, 400:500] = np.ones((100,100), dtype=np.float32)
+        adjacency_matrix[0:400, 400:500] = 2 * np.ones((400, 100), dtype=np.float32)
+        adjacency_matrix[400:500, 0:400] = 2 * np.ones((100, 400), dtype=np.float32)
+        # Kill an entry with probability 1-p: allegedly not correct but it's the first attempt
+        # But first set the seed for reproducibility
+        np.random.seed(42)
+        for i in track(range(500), description="Killing strangers"):
+            for j in range(i, 500):
+                if np.random.uniform(0,1) > 0.2:
+                    adjacency_matrix[i,j] = 0.0
+                    adjacency_matrix[j,i] = 0.0
+
+
+        np.fill_diagonal(adjacency_matrix , 0.0)
+
+        ## Yes I didn't know how to spell adjaajahecncy
+        self.net = undNetwork.from_adiacence(adjacency_matrix)
         self.net.initialize_embedding(dim=2)
         self.net.cMDE([1.0, 0.5, 0.1], [0.5, 0.01, 0.0], [10, 200, 500])
 
@@ -28,7 +49,7 @@ class Normal(undNetwork):
     
     def update(self):
 
-        print(f"Elapsed time: {self.time: .2f} us")
+        print(f"Elapsed time: {self.time: .1f} us")
 
         for i, node in enumerate(self.net):
 
@@ -40,18 +61,21 @@ class Normal(undNetwork):
 
         #### SUPER ALERT
         # BINNING IS MADE HERE
-        timesteps_per_frame = 1000
+        binning_time = 1_000 # 10 us
+        timesteps_per_frame = binning_time/TIMESTEP
 
-        for repeat in range(timesteps_per_frame):
+        for repeat in range(int(timesteps_per_frame)):
             try:
+                # However it went, increase the animation display time
+                # Must be stated before the dangerous operation 
+                # because catching the error prevents from tracing the timestep
+                self.time += 0.01 # One timestep (us)
+
                 # Tries to load a line from both files
                 # If weird shit happens just skips
                 # And weird shit can mean no firing nodes in that frame
                 firing_exc = np.array(json.loads(self.exc_file.readline())).reshape((-1,))
                 firing_inh = np.array(json.loads(self.inh_file.readline())).reshape((-1,))
-                
-                # However it went, increase the animation display time
-                self.time += 0.01 # One timestep (us)
 
             except json.JSONDecodeError:
                 continue
--- refactor.py
+++ refactor.py
@@ -6,26 +6,29 @@ from netgross.network import undNetwork
 from netgross import netplot
 import cnets
 
-ACTIVITY = "fullepileptic"
-NAME = "inhibitory"
+ACTIVITY = "metastable"
+NAME = "excitatory"
 FILE = "activityphases/{activity}activity_{name}.json".format(activity=ACTIVITY, name=NAME)
 
 with open(FILE) as ex:
-    for i, line in enumerate(ex):
-        times = json.loads(line)
-        print(f"line {i} done")
+    # The file is single-line
+    times_array = json.loads(ex.readline())
 
+frames = {}
 
-binning = np.empty(250_100, dtype=object)
-for _ in range(len(binning)):
-    binning[_] = []
-
-for neuron_index, times in track(enumerate(times), total=400):
+# The file is a list of lists of times so
+# Cycles over the node index (first list is first node)
+# And add this node to every frame in which it is active
+for neuron_index, times in track(enumerate(times_array), total=len(times_array)):
     for time in times:
         time_index = int(time*100)
-        binning[time_index].append(neuron_index)
-
+        already_firing = frames.get(time_index)
+        # If a neuron is already active at that time, appends
+        # else it starts the list
+        if already_firing is None:
+            frames[time_index] = [neuron_index]
+        else:
+            already_firing.append(neuron_index)
 
 with open(f"binned_{NAME}_{ACTIVITY}.json", 'w', encoding='utf-8') as f:
-    for timestep in track(binning, total=len(binning)):
-        json.dump(timestep, f, ensure_ascii=False, indent=4)
+    json.dump(frames, f, ensure_ascii=False, indent=4)
